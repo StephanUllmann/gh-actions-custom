fe72906569349c5e01e53284595d5935992fbcaa
Stephan Ullmann <stephan.ullmann@posteo.de>
2024-03-28 14:14:39 +0100

    custom deploy

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -1,7 +1,20 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+// const github = require('@actions/github');
 const exec = require('@actions/exec');
 
-function run() {}
-core.notice('Hello from my JS Action!');
+function run() {
+  // Get input values
+  const bucket = core.getInput('bucket', { required: true });
+  const bucketRegion = core.getInput('bucket-region', { required: true });
+  const distFolder = core.getInput('dist-folder', { required: true });
+
+  // Upload files to S3 with an aws cli command
+  const s3Uri = `s3://${bucket}`;
+
+  // AWS_ACCESS_KEY_ID will be looked up in the env;
+
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+  //
+  // core.notice('Hello from my JS Action!');
+}
 run();
